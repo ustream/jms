cc00589fd68fdf7d6df8abba6c1b51e11a4047af
_nec <nec@shell8.net>
2014-02-11 15:55:38 +0100

    cluster startup

---
+++ bin/jms.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var fs        = require('fs');
+var http      = require('http');
+var async     = require('async');
+
+var paths     = require('../conf/paths');
+var proc      = require(paths.confdir + '/proc');
+
+var checkdirs = require(paths.libdir + '/startup/checkdirs');
+var clusters  = require(paths.libdir + '/startup/clusters');
+
+// TODO php addscriptek egy request legyenek
+// TODO szebb filebetoltes :(
+
+// TODO: vezerles (status)
+
+// TODO: elozo buildnumber-ek is elerhetoek legyenek (failsafe)
+
+// TODO: releasetags / featurebranch kezeles
+
+
+
+
+async.waterfall([
+	checkdirs,
+	clusters,
+], function (err, result) {
+	// result now equals 'done'
+});
--- conf/paths.js
+++ conf/paths.js
@@ -1,10 +1,15 @@
 
 var paths = {
 
+	confdir: process.cwd() + "/conf",
+	libdir: process.cwd() + "/lib",
+
 	piddir: process.cwd() + "/pids",
-	logdir: process.cwd() + "/logs",
-	datadir: process.cwd() + "/data",
 	pidfile: process.cwd() + "/pids/cluster_pidfile",
+
+	datadir: process.cwd() + "/data",
+
+	logdir: process.cwd() + "/logs",
 	log: process.cwd() + "/logs/cluster.log",
 
 }
