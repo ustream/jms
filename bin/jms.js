53d5af2e001a59da4b21fca5a8025b1f59edd8bb
_nec <nec@shell8.net>
2014-02-21 10:54:06 +0100

    ditch forever in favor of pm2

--- bin/jms.js
+++ bin/jms.js
@@ -1,30 +1,5 @@
-#!/usr/bin/env node
 
-var fs        = require('fs');
-var http      = require('http');
-var async     = require('async');
+var paths    = require('../conf/paths');
+var ModuleServer = require(paths.libdir + '/server/moduleserver');
 
-var paths     = require('../conf/paths');
-var proc      = require(paths.confdir + '/proc');
-var log       = require(paths.libdir + '/debug/log');
-
-// TODO php addscriptek egy request legyenek
-
-// TODO: vezerles (status)
-
-// TODO: releasetags / featurebranch kezeles
-
-
-async.waterfall([
-	require(paths.libdir + '/startup/checkdirs'),
-	require(paths.libdir + '/startup/clusters'),
-], function (err, result) {
-
-
-	if (err) {
-		log.error('error accured');
-		throw err;
-	}
-
-	// result now equals 'done'
-});
+var server = new ModuleServer(function () {});
