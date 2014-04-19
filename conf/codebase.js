f24dafebec594d16098820974e9d8374f539b3f6
_nec <nec@shell8.net>
2014-04-19 15:47:46 +0200

    several fixes

--- conf/codebase.js
+++ conf/codebase.js
@@ -3,8 +3,6 @@ var codebase = {
 
 	jsrepo: paths.basedir + '/js',
 
-	init_app: 'loader',
-
     loadHidden: false,
 
 };
--- conf/network.js
+++ conf/network.js
@@ -1,8 +1,6 @@
 var network = {
 
 	host: '127.0.0.1',
-	//host: '62.112.194.33',
-	//host: '10.220.1.150',
 	port: 1337,
 
 	cache: true
