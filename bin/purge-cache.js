dcbbd8d037ba5ab8165880b13beab1fa2a93def4
_nec <nec@shell8.net>
2014-04-29 16:15:04 +0200

    add option to purge internal cache

---
+++ bin/purge-cache.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var paths          = require('../conf/paths');
+var storage        = require(paths.libdir + '/storage');
+
+
+if (process.argv[2]) {
+	deleteSingleKey(process.argv[2]);
+} else {
+	storage.hkeys('cache', deleteKeys);
+}
+
+function deleteSingleKey (key) {
+	storage.hdel('cache', [key], function (err, result) {
+		if (err) {
+			process.exit(1);
+			return;
+		}
+	});
+}
+
+function deleteKeys (err, keys) {
+
+	if (err) {
+		process.exit(1);
+		return;
+	}
+
+	if (!keys || keys.length < 1) {
+		process.exit(0);
+		return;
+	}
+
+	keys.forEach(deleteSingleKey);
+
+	process.exit(0);
+	return;
+}
+
