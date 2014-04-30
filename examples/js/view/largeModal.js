13b7493cc8b87664dca27163c56e29beb408e18e
_nec <nec@shell8.net>
2014-04-30 11:20:20 +0200

    update dep tree hashing

--- examples/js/view/largeModal.js
+++ examples/js/view/largeModal.js
@@ -7,7 +7,7 @@ define([], function () {
 			'<div class="modal-content">' +
 				'<div class="modal-header">' +
 					'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-					'<h4 class="modal-title">Modal title updated</h4>' +
+					'<h4 class="modal-title">Superb modal title</h4>' +
 				'</div>' +
 				'<div class="modal-body">' +
 					'<p>One fine body&hellip;</p>' +
