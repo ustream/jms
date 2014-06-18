74c77599a92a0381e5d287373c872602e879da2c
_nec <nec@shell8.net>
2014-06-18 14:40:44 +0200

    add tests #12

--- bin/jms.js
+++ bin/jms.js
@@ -1,10 +1,6 @@
-
 var paths    = require('../lib/paths');
 var ModuleServer = require(paths.libdir + '/server/moduleserver');
 
-
-
 var argv = require('minimist')(process.argv.slice(2));
-
 
 var server = new ModuleServer(function () {});
